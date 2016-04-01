5ebc03ad09fd0764725b64089f4e1cf81c3b9190
Ivan Milutinović <milutinovici@gmail.com>
2016-04-01 15:09:43 +0200

    added more performance tests

--- perf/test.ts
+++ perf/test.ts
@@ -16,6 +16,27 @@ suite.add("ComputedList#filter", () => {
         for (let i = 0; i < 100; i++) {
             list.push(i);
         }
+    })
+    .add("ComputedList#some", () => {
+        const list = px.list([]);
+        const even = list.someList(x => x % 2 === 0);
+        for (let i = 0; i < 100; i++) {
+            list.push(i);
+        }
+    })
+    .add("ComputedList#every", () => {
+        const list = px.list([]);
+        const even = list.everyList(x => x % 2 === 0);
+        for (let i = 0; i < 100; i++) {
+            list.push(i);
+        }
+    })
+    .add("ComputedList#reduce", () => {
+        const list = px.list([]);
+        const max = list.reduceList((x, y) => x > y ? x : y, undefined);
+        for (let i = 0; i < 100; i++) {
+            list.push(i);
+        }
     });
 
 export = suite;
