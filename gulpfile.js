516bd2c461048f0c5bda780d9244ba3891291ed9
Ivan Milutinović <milutinovici@gmail.com>
2016-04-08 18:31:31 +0200

    added release task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,28 @@
 const gulp = require("gulp");
+const merge = require("merge2");
 const jasmine = require("gulp-jasmine");
-const ts = require("gulp-typescript");
+const gulpTs = require("gulp-typescript");
 const sourceMaps = require("gulp-sourcemaps");
 const benchmark = require("gulp-benchmark");
+const typescript = require("typescript");
  
-
-const tsProject = ts.createProject("tsconfig.json");
-
-gulp.task("default",  () => tsProject.src()
-                            .pipe(sourceMaps.init())
-                            .pipe(ts(tsProject))
-                            .pipe(sourceMaps.write())
-                            .pipe(gulp.dest("."))
-                            );
+gulp.task("default",  () => {
+    const project = gulpTs.createProject("tsconfig.json");
+    return project.src().pipe(sourceMaps.init())
+                        .pipe(gulpTs(project))
+                        .pipe(sourceMaps.write())
+                        .pipe(gulp.dest("."));
+});
  
-gulp.task("test", () => gulp.src("spec/**/*.js")
-                            .pipe(jasmine())
-                            );
+gulp.task("test", () => gulp.src("spec/**/*.js").pipe(jasmine()));
 
-gulp.task("bench",() => gulp.src('perf/**/*.js', {read: false})
-                            .pipe(benchmark())
-                            );
+gulp.task("bench",() => gulp.src("perf/**/*.js", {read: false}).pipe(benchmark()));
+
+gulp.task("release",  () => {
+    const project = gulpTs.createProject({ outFile: "proactive.js", module: "amd", declaration: true, typescript: typescript });
+    const result = gulp.src("src/**/*.ts").pipe(gulpTs(project));
+    return merge([
+		result.dts.pipe(gulp.dest("./dist")),
+		result.js.pipe(gulp.dest("./dist"))
+	]);
+});
