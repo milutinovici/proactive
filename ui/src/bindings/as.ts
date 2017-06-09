ccd04da51d986442bb174fa1a314035ad7c718f5
Ivan Milutinović <milutinovici@gmail.com>
2017-06-09 14:09:22 +0200

    as can be used in conjunction with component binding

--- ui/src/bindings/as.ts
+++ ui/src/bindings/as.ts
@@ -1,26 +1,28 @@
 import { Observable } from "rxjs";
 import { BaseHandler } from "./baseHandler";
-import { INodeState, Parametricity, IBinding } from "../interfaces";
+import { INodeState, IBinding } from "../interfaces";
 import { DomManager } from "../domManager";
 
 export class AsBinding<T> extends BaseHandler<T> {
     constructor(name: string, domManager: DomManager) {
         super(name, domManager);
         this.priority = 50;
-        this.parametricity = Parametricity.Required;
+        // this.parametricity = Parametricity.Required;
         // this.controlsDescendants = true;
     }
 
     public applyInternal(element: HTMLElement, binding: IBinding<T>, state: INodeState) {
         const observable = binding.evaluate(state.context, this.dataFlow) as Observable<T>;
         const name = binding.parameter as string;
-        // subscribe
-        binding.cleanup.add(observable.subscribe(model => {
-            state.context = state.context.extend(name, model);
+        if (name) {
+            // subscribe
+            binding.cleanup.add(observable.subscribe(model => {
+                state.context = state.context.extend(name, model);
 
-            this.domManager.cleanDescendants(element);
-            this.domManager.applyBindingsToDescendants(state.context, element);
-        }));
+                this.domManager.cleanDescendants(element);
+                this.domManager.applyBindingsToDescendants(state.context, element);
+            }));
+        }
     }
 
 }
