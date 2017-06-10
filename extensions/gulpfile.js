d127ade5f49b0f447aeae75dbe7137ab184f9a92
Ivan Milutinović <milutinovici@gmail.com>
2017-06-10 19:02:10 +0200

    changed build

--- extensions/gulpfile.js
+++ extensions/gulpfile.js
@@ -2,33 +2,23 @@ const gulp = require("gulp");
 const tape = require("gulp-tape");
 const spec = require("tap-spec");
 const benchmark = require("gulp-benchmark");
-const uglify = require("gulp-uglify");
-const browserify = require("browserify");
-const source = require("vinyl-source-stream");
-const buffer = require("vinyl-buffer");
-const tsify = require("tsify");
+const ts = require("gulp-typescript");
+const merge = require("merge2");
 
-const vendors = ["rxjs"];
-
-gulp.task("build",  () => 
-    browserify({ debug: true })
-    .external(vendors)
-    .add("src/extensions.ts")
-    .plugin(tsify).bundle()
-    .pipe(source("extensions.js"))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest("./dist/"))
+gulp.task("build",  () => {
+        const project = ts.createProject("tsconfig.json");
+        const result = project.src().pipe(project());
+        return merge([
+            result.dts.pipe(gulp.dest("./lib")),
+            result.js.pipe(gulp.dest("./lib"))
+        ]);
+    }
 );
  
-gulp.task("test", () => 
-    browserify({ debug: true })
-    .add("spec/spec.ts")
-    .plugin(tsify).bundle()
-    .pipe(source("spec.js"))
-    .pipe(buffer())
-    .pipe(gulp.dest("./dist/"))
-    .pipe(tape({ reporter: spec() }))
+gulp.task("test", () => {
+    const result = gulp.src("./lib/spec/*.js")
+                       .pipe(tape({ reporter: spec() }));
+    }
 );
 
-gulp.task("bench", () => gulp.src("./dist/perf/**/*.js", {read: false}).pipe(benchmark()));
+gulp.task("bench", () => gulp.src("./lib/perf/**/*.js", {read: false}).pipe(benchmark()));
