f30985892a6d9deb2a32bea829fd04464f8f7051
Ivan Milutinović <milutinovici@gmail.com>
2017-07-31 17:45:33 +0200

    update dependencies, add sourcemaps

--- extensions/gulpfile.js
+++ extensions/gulpfile.js
@@ -4,16 +4,19 @@ const spec = require("tap-spec");
 const benchmark = require("gulp-benchmark");
 const ts = require("gulp-typescript");
 const merge = require("merge2");
+const sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("build",  () => {
-        const project = ts.createProject("tsconfig.json");
-        const result = project.src().pipe(project());
-        return merge([
-            result.dts.pipe(gulp.dest("./lib")),
-            result.js.pipe(gulp.dest("./lib"))
-        ]);
-    }
-);
+    const project = ts.createProject("tsconfig.json");
+    const result = project.src()
+        .pipe(sourcemaps.init())  
+        .pipe(project());
+
+    return merge([
+        result.dts.pipe(gulp.dest("./lib")),
+        result.js.pipe(sourcemaps.write()).pipe(gulp.dest("./lib"))
+    ]);
+});
  
 gulp.task("test", () => {
     const result = gulp.src("./lib/spec/*.js")
