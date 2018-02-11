668588a26a95ff0ac6dac9a22d60cec2dd356210
milutinovici <milutinovici@gmail.com>
2018-02-11 18:18:58 +0100

    group tests

--- ui/spec/spec.ts
+++ ui/spec/spec.ts
@@ -4,10 +4,12 @@
 /////////////////////////
 import "./directives/text-spec";
 import "./directives/attr-spec";
-import "./directives/component-spec";
 import "./directives/css-spec";
 import "./directives/event-spec";
-import "./directives/if-spec";
+
 import "./directives/value-spec";
 import "./directives/checked-spec";
+
+import "./directives/if-spec";
+import "./directives/component-spec";
 import "./directives/for-spec";
